495eba190e3a35473485b6c311ccb6b7645cb83f
Olivier Burggraaff <olivierburggraaff@gmail.com>
2018-01-13 01:36:38 +0100

    Split players

---
+++ beavers/core/players/ai.py
@@ -0,0 +1,5 @@
+from .player import Player
+
+
+class AI(Player):
+    pass
--- beavers/core/players/human.py
+++ beavers/core/players/human.py
@@ -1,24 +1,4 @@
-# -*- coding: utf-8 -*-
-
-from .cards import Hand
-
-
-class Player(object):
-    def __init__(self, name):
-        self.name = name
-        self.hand = None
-
-    def deal_hand(self, cards, stack):
-        self.hand = Hand(cards, stack)
-
-    def __repr__(self):
-        return f"{self.name} ({self.__class__.__name__})"
-
-    def score(self):
-        return self.hand.score()
-
-    def play(self, r):
-        raise NotImplementedError(f"{self.__class__} has not implemented `play`")
+from .player import Player
 
 
 class Human(Player):
@@ -47,7 +27,3 @@ class Human(Player):
                 r.ender = self
                 print(f"{self.name} is ending the round!")
         print(self.hand, r.stack)
-
-
-class AI(Player):
-    pass
